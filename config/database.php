74d0566b102b5332ce3a7397a486d0fb7e434f90
ibrahim hamzat <hamat.ibrahim3@gmail.com>
2020-04-27 11:26:22 +0100

    db fix

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'pizza_fac'),
             'username' => env('DB_USERNAME', 'pizza_f'),
-            'password' => env('DB_PASSWORD', 'hamzat12'),
+            'password' => env('DB_PASSWORD', 'hamzat123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
